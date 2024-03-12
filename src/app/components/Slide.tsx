82465cc22915cbea3c54aa41636cadbe6ba42919
Markus Ecker <markus.ecker@gmail.com>
2024-03-12 18:13:13 +0100

    remove text area autocomplete

--- src/app/components/Slide.tsx
+++ src/app/components/Slide.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useCopilotAction } from "@copilotkit/react-core";
-import { CopilotTextarea } from "@copilotkit/react-textarea";
 
 export interface SlideModel {
   title: string;
@@ -87,7 +86,7 @@ export const Slide = (props: SlideProps) => {
         </div>
 
         <div className="h-2/3 flex">
-          <CopilotTextarea
+          <textarea
             className="w-1/2 text-3xl text-black font-medium p-10 resize-none bg-transparent m-12 rounded-xl"
             style={{
               lineHeight: "1.5",
@@ -96,18 +95,6 @@ export const Slide = (props: SlideProps) => {
             placeholder="Body"
             onChange={(e) => {
               props.partialUpdateSlide({ content: e.target.value });
-            }}
-            autosuggestionsConfig={{
-              textareaPurpose:
-                "The content of a slide in a presentation. Keep it short.",
-              chatApiConfigs: {
-                suggestionsApiConfig: {
-                  forwardedParams: {
-                    max_tokens: 20,
-                    stop: [".", "?", "!"],
-                  },
-                },
-              },
             }}
           />
 
@@ -122,7 +109,7 @@ export const Slide = (props: SlideProps) => {
         </div>
       </div>
 
-      <CopilotTextarea
+      <textarea
         className=" w-9/12 h-full bg-transparent text-5xl p-10 resize-none bg-gray-500 pr-36"
         style={{
           height: `${heightOfSpeakerNotes}px`,
@@ -136,17 +123,6 @@ export const Slide = (props: SlideProps) => {
         value={props.slide.spokenNarration}
         onChange={(e) => {
           props.partialUpdateSlide({ spokenNarration: e.target.value });
-        }}
-        autosuggestionsConfig={{
-          textareaPurpose: "The spoken narration of the slide.",
-          chatApiConfigs: {
-            suggestionsApiConfig: {
-              forwardedParams: {
-                max_tokens: 20,
-                stop: [".", "?", "!"],
-              },
-            },
-          },
         }}
       />
     </>
